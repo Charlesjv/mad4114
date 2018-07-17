158b8fdb165c71db6e4a58907c76c45cc69f4d6b
Charlesjv <35268266+Charlesjv@users.noreply.github.com>
2018-07-17 12:01:11 -0400

    Add files via upload

---
+++ jenellecestar-thenotebook-4c2c4b2f5d81/TheNotebook/tTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  tTableViewCell.swift
+//  TheNotebook
+//
+//  Created by MacStudent on 2018-07-17.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class tTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
